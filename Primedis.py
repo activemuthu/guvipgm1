0f390bd1cdf49dc697ebc0b09a26ae82cf8521c7
activemuthu <50619703+activemuthu@users.noreply.github.com>
2019-05-19 11:51:12 +0530

    Update Primedis.py

--- Primedis.py
+++ Primedis.py
@@ -1,10 +1,10 @@
-h, m =input().split())
+h, m =input().split()
 h=int(h) 
 m=int(m) 
 for j in range(h+1,m):
-  S= 0
+  S=0
   for c in range(2,j):
     if j % c == 0:
       S= S+ 1
-  if S == 0:
-      print (j,end="")
+  if S==0:
+      print(j,end="")
